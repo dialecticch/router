c332ddfc47f8670cca63ed9c16463aacdb52fef2
decanus <7621705+decanus@users.noreply.github.com>
2021-08-31 13:31:48 +0200

    fixes

--- contracts/Interfaces/ERC20.sol
+++ contracts/Interfaces/ERC20.sol
@@ -2,5 +2,6 @@ pragma solidity ^0.8.4;
 
 interface ERC20 {
     function transferFrom(address, uint256) external returns (bool);
-    function approve(address, address, uint256) external returns (bool);
+
+    function approve(address, uint256) external returns (bool);
 }
--- contracts/Router.sol
+++ contracts/Router.sol
@@ -1,23 +1,54 @@
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
 import "./Interfaces/ERC20.sol";
 import "./Interfaces/UniswapV2.sol";
 
+/// @title SwapRouter
+/// @dev A simple wrapper that selects the exchange with the best price and trades tokens on it.
+/// @author Dialectic
 library SwapRouter {
+    /// @dev Emitted after a trade occurs.
+    /// @param exchange The exchange tokens were swapped through.
+    /// @param input The input token.
+    /// @param output The received token.
+    /// @param amountIn The amount of input tokens traded.
+    /// @param amountOut The amount of output tokens received.
+    event Swapped(address indexed exchange, address indexed input, address indexed output, uint256 amountIn, uint256 amountOut);
 
-    function swap(UniswapV2[] swaps, address[] memory path, uint256 amount, uint256 slippage) internal {
+    /// @dev Swaps tokens through a specified path on the best exchange.
+    /// @param swaps The exchanges to choose from.
+    /// @param path The path for the swap.
+    /// @param amount The amount of tokens to sell.
+    /// @param slippage The max slippage in basis points.
+    /// @return The amount of tokens received.
+    function swap(
+        UniswapV2[] swaps,
+        address[] memory path,
+        uint256 amount,
+        uint256 slippage
+    ) internal returns (uint256) {
         (UniswapV2 swap, uint256 amountOut) = dataForBestExchange(swaps, path, amount);
 
-        swap.swapExactTokensForTokens(
+        ERC20(path[0]).approve(address(swap), amount);
+
+        uint256[] memory amountsOut = swap.swapExactTokensForTokens(
             amount,
             (amountOut * (1000 - slippage)) / 1000,
             path,
             address(this),
             block.timestamp + 180
         );
+
+        uint256 out = amountsOut[amountsOut.length - 1];
+        emit Swapped(address(swap), path[0], path[path.length - 1], amount, out);
+        return out;
     }
 
-    function dataForBestExchange(UniswapV2[] swaps, address[] memory path, uint256 amount) private view returns (UniswapV2, uint256) {
+    function dataForBestExchange(
+        UniswapV2[] swaps,
+        address[] memory path,
+        uint256 amount
+    ) private view returns (UniswapV2, uint256) {
         UniswapV2 bestExchange;
         uint256 bestAmount;
 
@@ -44,5 +75,4 @@ library SwapRouter {
             return 0;
         }
     }
-
 }
